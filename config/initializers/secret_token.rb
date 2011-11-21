4512b173bc0e1366016c356215d28a29dbc57fbb
Vasily Myazin <vm@konspiredesign.com>
2011-11-20 23:38:43 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DietMatch::Application.config.secret_token = '83295aa766a2eb3f479bf1dea42f45350324dbdfce4b5c1df76ceaaf7f3c664539aa60aff26a48897c5047fffb698437d8ead3c402954eb43f86003138940aba'

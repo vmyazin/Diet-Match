96c74903a39220e0126d996ca2156e3afe7c56df
Vasily Myazin <vm@konspiredesign.com>
2011-12-03 00:07:16 -0800

    Added some gems

--- db/migrate/20111116083102_create_posts.rb
+++ db/migrate/20111116083102_create_posts.rb
@@ -1,15 +1,14 @@
-class CreatePosts < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def self.up
-    create_table :posts do |t|
+    create_table :users do |t|
       t.string :name
-      t.string :title
-      t.text :content
+      t.string :email
 
       t.timestamps
     end
   end
 
   def self.down
-    drop_table :posts
+    drop_table :users
   end
 end
